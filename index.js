e35fe585030b6c8c28b4acdc3a6e21aea8689076
Lornah Mukiri <mukirikabuli@gmail.com>
2022-06-23 20:01:24 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+const drivers = [{name: 'Brenda', hometown: 'Mombasa'}, {name: 'Kendi', hometown: 'Nyeri'}]
+
+const names = ['Angela', 'becky', 'MOFFAT'];
+
+function findMatching (driverArray, driverName) {
+    const driverNameUpper = driverName.toUpperCase();
+    const matchedNames = driverArray.filter (driver => driver.toUpperCase() === driverNameUpper);
+    return matchedNames;
+}
+
+
+
+function fuzzyMatch (driverArray, string) {
+    const fuzzyNames = driverArray.filter (function (driver) {
+        return driver.startsWith(string);
+    })
+    return fuzzyNames;
+}
+
+
+function matchName (driverObjects, string) {
+    const matchedNames = driverObjects.filter (function(driver){
+        return driver.name === string;
+    })
+    return matchedNames;
+} 
